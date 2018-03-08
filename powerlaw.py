e3881ce3956b16841a6f10a83e0684fe8734799e
Harit <slipknot.opal@gmail.com>
2018-03-09 01:39:27 +0700

    power law

---
+++ powerlaw.py
@@ -0,0 +1,52 @@
+import numpy
+import matplotlib.pyplot as plt
+from copy import deepcopy
+from PIL import Image
+from math import cos, sin
+
+
+def getGrayColor(rgb):
+    return rgb[0]
+
+
+def setGrayColor(color):
+    return [color, color, color]
+
+
+img = Image.open('Lena.png')
+img = numpy.asarray(img)
+
+c = 1
+y = float(input("input y :"))
+
+# copy list not reference
+pwl = deepcopy(img)
+
+
+min = 99999999999999
+max = 0
+
+for i in range(len(img)):
+    for j in range(len(img[i])):
+        s = (c*img[i][j][0])**y
+        if(s > max):
+            max = s
+        if(s < min):
+            min = s
+
+for i in range(len(img)):
+    for j in range(len(img[i])):
+        s = (c*img[i][j][0])**y
+        s = (s-min)/(max-min)
+        s = s*255
+        pwl[i][j] = setGrayColor(s)
+
+print('min:',min,'max:',max)
+
+plt.subplot(2, 2, 1)
+plt.imshow(img)
+plt.subplot(2, 2, 2)
+plt.imshow(pwl)
+
+
+plt.show()
